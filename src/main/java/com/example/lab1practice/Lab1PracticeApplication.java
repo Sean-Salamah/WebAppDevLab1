8c03fe1a68d5a370fa88b15fa44f5d204cea36b7
Sean-Salamah <seanhsalamah@gmail.com>
2026-01-16 11:41:49 +0000

    1st spring boot commit

---
+++ src/main/java/com/example/lab1practice/Lab1PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab1practice/Lab1PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab1practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab1PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
