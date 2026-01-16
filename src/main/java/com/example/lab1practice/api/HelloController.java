9bf2871b735122cfee442e28153319c907e147ed
Sean-Salamah <seanhsalamah@gmail.com>
2026-01-16 12:26:01 +0000

    Added a chaning greeting message depending on the url, hello/yournamehere

--- src/main/java/com/example/lab1practice/api/HelloController.java
+++ src/main/java/com/example/lab1practice/api/HelloController.java
@@ -1,12 +1,20 @@
 package com.example.lab1practice.api;
 
+import com.example.lab1practice.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello from Spring Boot";
-    }
+ private final GreetingService service;
+
+ public HelloController (GreetingService service) {
+     this.service = service;
+ }
+
+ @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable String name){
+     return service.getGreeting(name);
+ }
 }
